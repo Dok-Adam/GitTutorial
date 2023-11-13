1541238ae663444b404148703f57268f9fab9154
Adam <doktoradam72@gmail.com>
2023-11-13 22:04:45 +0500

    add model and modelviewmodel

---
+++ Git/Model/View/ModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelView.swift
+//  Git
+//
+//  Created by Адам Дотмерзаев on 13.11.2023.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelView()
+}
---
+++ Git/Model/ViewModel/ModelViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ModelViewModel.swift
+//  Git
+//
+//  Created by Адам Дотмерзаев on 13.11.2023.
+//
+
+import SwiftUI
+
+struct ModelViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelViewModel()
+}
